41c78adaaf76a3e954c2e057ad39ca7532ca1e66
suzonmahbub <suzonmahbubdev.com>
2025-03-04 00:16:22 +0600

    javascript file updated

--- script.js
+++ script.js
@@ -1,14 +1,45 @@
-function completeTask(id) {
+let count = 0;
+let taskAssign = document.getElementById('taskAssign');
+let totalTask = document.getElementById('totalTask');
+const history1 = document.getElementById('history');
+const history = [];
+const currentDate = document.getElementById('current-date');
+const currentDay = document.getElementById('current-day');
+
+currentDay.innerHTML = `${new Date().toDateString().slice(0, 3)} ,`
+
+
+currentDate.innerHTML = new Date().toDateString().slice(3)
+
+function completeTask(id, text) {
     alert("Board updated successfully");
     const button = document.getElementById(id);
-
+    console.log(text)
     if (button) {
         button.disabled = true;
         button.innerText = "completed"
     }
-    let taskAssign = document.getElementById('taskAssign');
-    let totalTask = document.getElementById('totalTask');
+    if (text) {
+        history.push(text)
+    }
+    console.log(history)
+
+    count++
+    if (count === 6) {
+        alert('all completed done')
+    }
+    for (let i = 0; history.length > i; i++) {
+        history1.innerHTML = ` <p>${history[i]}</p>`
+    }
+
 
     taskAssign.innerText = parseInt(taskAssign.innerText) - 1;
-    totalTask.innerText = parseInt(totalTask.innerText) + 1;  
+    totalTask.innerText = parseInt(totalTask.innerText) + 1;
+
+
+}
+
+function clearHistory() {
+    count === 0;
+    history1.innerText = ''
 }
